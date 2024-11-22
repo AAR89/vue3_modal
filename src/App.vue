687326b53b0b754453b63718ed2932f443ff6ee2
Alex Ur <ru.booking@yandex.ru>
2024-11-22 15:39:24 +0300

    add modal component

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,30 @@
-<script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
-import BaseComponent from './components/BaseComponent.vue'
-</script>
-
 <template>
-  <BaseComponent />
+  <div>
+    <button @click="showModal">Открыть модальное окно</button>
+    <Modal ref="modal">
+      <h2>Это модальное окно!</h2>
+      <p>Здесь может быть ваше содержимое.</p>
+    </Modal>
+  </div>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue'
+import Modal from './components/ModalComponent.vue'
+
+const modal = ref(null)
+
+function showModal() {
+  modal.value?.showModal()
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
 }
 </style>
